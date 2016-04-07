877e28b677a67e6783388ddcdc351ce84d2375da
Aleksey Buzmakov <aleksey.buzmakov@yandex.ru>
2016-04-07 12:19:16 +0500

    some scripts for windows building

--- update-makefiles.bash
+++ update-makefiles.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd $(dirname "$0")
 cbp2make -in Sofia-PS/sofia-ps.cbp -out Sofia-PS/makefile -targets "Debug,Release" -unix
+cbp2make -in Sofia-PS/sofia-ps.cbp -out Sofia-PS/makefile.win -targets "Debug,Release" -windows
