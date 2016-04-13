e4c9cadca789b36b3d4db1e6c4b63c4dbeca31d1
Aleksey Buzmakov <aleksey.buzmakov@yandex.ru>
2016-04-13 10:45:58 +0500

    Modifying path to libraries. Correcting README for semi-manual instalation

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Some boost libraries (regex, thread, system, filesystem) should be compiled:
 
 * Then build all libraries
 
-> $ ./b2 release
+> $ ./b2 release stage
 
 * _If you want a faster compiling please read instructions from the git library_
 
@@ -44,7 +44,10 @@ The most widely used configurations are
 
 Basically this script fetches [rapidjson](https://github.com/miloyip/rapidjson.git) and apply the file rapidjson.patch.
 Then it converts the description in the file __premake4.lua__ to the format of your IDE in love.
-Then, I guess, you know what to do with the resulting file
+Then, I guess, you know what to do with the resulting file.
+
+* *For the moment, on some configurations, e.g., Windows and MS Visual Studio, a manual naming of static linked libraries is needed. This libraries are found in boost/stage/libs, but there names are not fixed, They are named like "lib{BOOST LIB NAME}-bla-bla-bla".{a|lib}, for example 'libboost_regex-gcc34-mt-d-1_36.a'*
+* *Be careful, the boost libraries should be compiled by the same toolchain as the main program. For instance it is not possible to compile the boost libraries by the MSVS2015 toolchain and the program by the MSVS2013 toolchain. Errors of linkage occur in this case.* 
 
 The result can be found in __bin/__.
 
--- premake4.lua
+++ premake4.lua
@@ -2,66 +2,64 @@
 
 -- A solution contains projects, and defines the available configurations
 solution "Sofia-PS"
-   configurations { "Release", "Debug" }
+configurations { "Release", "Debug" }
 
-   -- A project defines one build target
-   project "Sofia-PS"
-      kind "ConsoleApp"
-      language "C++"
-      includedirs { 
-	      "boost/", -- There is no search for the include dirs (in particular on windows it is prety difficult
-	      "rapidjson/include",
-	      "FCAPS/include/", 
-	      "FCAPS/src/",
-	      "Tools/", 
-	      "../LibGastonForSofia/inc", 
-	      "Tools/" 
-      }
-      excludes { "FCAPS/src/fcaps/premodules/**", "rapidjson/**", "boost/**" }
-      files { "**.h", "**.inl", "**.cpp" }
+-- A project defines one build target
+project "Sofia-PS"
+kind "ConsoleApp"
+language "C++"
+includedirs { 
+	"boost/", -- There is no search for the include dirs (in particular on windows it is prety difficult
+	"rapidjson/include",
+	"FCAPS/include/", 
+	"FCAPS/src/",
+	"Tools/", 
+	"../LibGastonForSofia/inc", 
+	"Tools/" 
+}
+excludes { "FCAPS/src/fcaps/premodules/**", "rapidjson/**", "boost/**" }
+files { "**.h", "**.inl", "**.cpp" }
 
-      bindir = "bin/"
-	  
-	  rapidjsonDir="rapidjson/"
-	  if ( not os.isdir(rapidjsonDir)  ) then
-		os.execute( "git clone https://github.com/miloyip/rapidjson.git " .. rapidjsonDir )
-		os.execute( "git --git-dir " .. rapidjsonDir .. ".git --work-tree " .. rapidjsonDir .. " reset --hard c745c953adf68" )
-		os.execute( "git --git-dir " .. rapidjsonDir .. ".git --work-tree " .. rapidjsonDir .. " apply --whitespace=fix rapidjson.patch" )
-	  end
-	  
-	  libdirs {
-		 "boost/bin.v2/libs/filesystem/build/*/release/link-static/threading-multi/",
-		 "boost/bin.v2/libs/system/build/*/release/link-static/threading-multi/",
-		 "boost/bin.v2/libs/thread/build/*/release/link-static/threading-multi/"
-	  }
+bindir = "bin/"
 
-      configuration "Debug"
-         debugBindir= bindir .. "Debug/"
-	 os.mkdir(debugBindir)
-      	 targetdir( debugBindir )
-	 targetname( "Sofia-PS-D" )
-         defines { "DEBUG" }
-         flags { "Symbols" }
-	 links{ 
-		 "boost_filesystem",
-		 "boost_system",
-		 "boost_thread",
-		 "dl",
-		 "pthread"
-	 }
+libdirs {
+	"boost/stage/libs/",
+}
 
-      configuration "Release"
-         releaseBindir= bindir .. "Release/"
-	 os.mkdir(releaseBindir)
-      	 targetdir( releaseBindir )
-	 targetname( "Sofia-PS" )
-         defines { "NDEBUG", "BOOST_DISABLE_ASSERTS" }
-         flags { "Optimize" }
-	 links{ 
-		 "boost_filesystem",
-		 "boost_system",
-		 "boost_thread",
-		 "dl",
-		 "pthread"
-	 }
+rapidjsonDir="rapidjson/"
+if ( not os.isdir(rapidjsonDir)  ) then
+	os.execute( "git clone https://github.com/miloyip/rapidjson.git " .. rapidjsonDir )
+	os.execute( "git --git-dir " .. rapidjsonDir .. ".git --work-tree " .. rapidjsonDir .. " reset --hard c745c953adf68" )
+	os.execute( "git --git-dir " .. rapidjsonDir .. ".git --work-tree " .. rapidjsonDir .. " apply --whitespace=fix rapidjson.patch" )
+end
+
+configuration "Debug"
+debugBindir= bindir .. "Debug/"
+os.mkdir(debugBindir)
+targetdir( debugBindir )
+targetname( "Sofia-PS-D" )
+defines { "DEBUG" }
+flags { "Symbols" }
+links{ 
+	"boost_filesystem",
+	"boost_system",
+	"boost_thread",
+	"dl",
+	"pthread"
+}
+
+configuration "Release"
+releaseBindir= bindir .. "Release/"
+os.mkdir(releaseBindir)
+targetdir( releaseBindir )
+targetname( "Sofia-PS" )
+defines { "NDEBUG", "BOOST_DISABLE_ASSERTS" }
+flags { "Optimize" }
+links{ 
+	"boost_filesystem",
+	"boost_system",
+	"boost_thread",
+	"dl",
+	"pthread"
+}
 
