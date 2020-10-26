5cb825e088f05486a0a25b101b7574cc308aa1fa
Aleksey Buzmakov <aleksey.buzmakov@yandex.ru>
2020-10-26 13:44:05 +0500

    block allocator

---
+++ FCAPS/src/fcaps/SharedModulesLib/BlockAllocator.cpp
@@ -0,0 +1,122 @@
+// Initial software, Aleksey Buzmakov, Copyright (c) INRIA and University of Lorraine, GPL v2 license, 2020, v0.8
+
+#include "BlockAllocator.h"
+
+using namespace std;
+
+void CBlockAllocator::Reserve( size_t blockCount )
+{
+	if( GetAvailableBlockCount() >= blockCount ) {
+		return;
+	}
+	allocate( blockCount );
+}
+size_t CBlockAllocator::GetAvailableBlockCount() const
+{
+	size_t totalBlockCount = 0;
+	auto itr = memory.begin();
+	for( ; itr != memory.end(); ++itr ) {
+		totalBlockCount += (*itr).size();
+		assert( ((*itr).size() % blockSize ) == 0 );
+	}
+	return totalBlockCount / blockSize;
+}
+
+// Allocates new memory
+inline void CBlockAllocator::allocate()
+{
+	DWORD s = 0;
+	if(memory.empty() == 0) {
+		s=1000;
+	}else{
+		const DWORD lastS = memory.back().size() / blockSize;
+		const DWORD criticalSize = 1024*1024*100;
+		// TOCHANGE : Exp is so power that in certain moment we can be out of memory because of such a multiplication
+		s = min<DWORD>( lastS*2, criticalSize / sizeof(TElementType) / blockSize);
+	}
+	allocate( s );
+}
+
+// Allocates the requeested amount of blocks
+void CBlockAllocator::allocate( DWORD count )
+{
+	memory.push_back( CMemoryBlock() );
+	memory.back().resize( count * blockSize );
+
+	TElementType* ptr = &memory.back().front();
+#ifndef NDEBUG
+	// memeseting memory only in debug
+	memset( ptr, 0xFA, count * blockSize * sizeof(TElementType) );
+#endif // NDEBUG
+
+	for( DWORD i = 0; i < count - 1; ++i ) {
+		*ptr = reinterpret_cast<TElementType>( ptr + blockSize );
+		assert( checkMemory( ptr + blockSize, true ) );
+		ptr += blockSize;
+	}
+	*ptr = reinterpret_cast<TElementType>( nextFreeBlock );
+
+	nextFreeBlock = &memory.back().front();
+	assert( checkMemory( nextFreeBlock, true ) );
+}
+
+// Checks if memory is within the allocator
+bool CBlockAllocator::checkMemory( const TElementType* ptr, bool startOfBlock ) const
+{
+	auto itr = memory.begin();
+	for( ; itr != memory.end(); ++itr ) {
+		if( &(*itr).front() <= ptr && ptr < (&(*itr).back()+ 1) ) {
+			if( !startOfBlock ) {
+				return true;
+			} else {
+				const bool rslt = ptr +blockSize <= (&(*itr).back() + 1)
+					&& ( (reinterpret_cast<TElementType>(ptr)
+						-reinterpret_cast<TElementType>(&(*itr).front())) % (blockSize*sizeof(TElementType)) ) == 0;
+				// For breakpoitns on false.
+				if( rslt ) {
+					return true;
+				} else {
+					return false;
+				}
+
+			}
+		}
+	}
+	return false;
+}
+
+// Allocates new block and solves problem with memory.
+CBlockAllocator::TElementType* CBlockAllocator::newBlock( bool clear )
+{
+	++allocatedBlocks;
+
+	if( nextFreeBlock == 0 ) {
+		allocate();
+	}
+	assert( nextFreeBlock != 0 );
+	assert( checkMemory( nextFreeBlock, true ) );
+
+	TElementType* const nextBlock = reinterpret_cast<TElementType*>( *nextFreeBlock );
+	assert( nextBlock == 0 || checkMemory(nextBlock, true ) );
+#ifndef NDEBUG
+	memset( nextFreeBlock, 0, blockSize * sizeof(TElementType) );
+#else
+	if( clear ) {
+		memset( nextFreeBlock, 0, blockSize * sizeof(TElementType) );
+	}
+#endif // NDEBUG
+
+	return nextBlock;
+}
+
+// Frees the block
+void CBlockAllocator::freeBlock( TElementType* ptr )
+{
+	--allocatedBlocks;
+
+	assert( checkMemory( ptr, true ) );
+	assert( nextFreeBlock == 0 || checkMemory( nextFreeBlock, true ) );
+
+	*ptr = reinterpret_cast<TElementType>( nextFreeBlock );
+	nextFreeBlock = ptr;
+}
---
+++ FCAPS/src/fcaps/SharedModulesLib/BlockAllocator.h
@@ -0,0 +1,65 @@
+// Initial software, Aleksey Buzmakov, Copyright (c) INRIA and University of Lorraine, GPL v2 license, 2020, v0.8
+
+#ifndef CBLOCKALLOCATOR_H
+#define CBLOCKALLOCATOR
+
+#include <common.h>
+
+#include <vector>
+#include <list>
+#include <stdint.h>
+
+class CBlockAllocator {
+	typedef uintptr_t TElementType;
+public:
+	CBlockAllocator() :
+		blockSize(4), nextFreeBlock(0), allocatedBlocks(0) {}
+
+	// Get the size of the basic block
+	static size_t GetElementSize()
+		{return sizeof(TElementType);}
+
+	// Get/Set the size of blocks is ints
+	size_t GetBlockSize() const
+		{return blockSize;}
+	// Can be called only when no blocks are allocated
+	void SetBlockSize(size_t size)
+		{assert(allocatedBlocks == 0); blockSize = size;}
+
+	// Reserve memory for at least n elements
+	void Reserve( size_t blockCount );
+	size_t GetAvailableBlockCount() const;
+	size_t GetMemoryConsumption() const
+		{ return allocatedBlocks * blockSize * sizeof(TElementType); }
+	size_t GetTotalMemoryConsumption() const
+		{ return GetAvailableBlockCount() * blockSize * sizeof(TElementType); }
+
+	// New and free operations on top of the block alocator
+	TElementType* New()
+		{ return newBlock(false); }
+	void Free( TElementType* ptr )
+		{freeBlock(ptr);}
+
+private:
+	// Memory allocator related types.
+	typedef std::vector<TElementType> CMemoryBlock;
+	typedef std::list<CMemoryBlock> CMemory;
+
+private:
+	// Size of the blocks in elements
+	size_t blockSize;
+	// Memory for storing data
+	CMemory memory;
+	// Pointer to the next free memory
+	uintptr_t* nextFreeBlock;
+	// Patterns allocated
+	size_t allocatedBlocks;
+
+	void allocate();
+	void allocate( DWORD count );
+	bool checkMemory( const uintptr_t* ptr, bool startOfBlock ) const;
+	TElementType* newBlock( bool clear );
+	void freeBlock( TElementType* ptr );
+};
+
+#endif // CBLOCKALLOCATOR_H
